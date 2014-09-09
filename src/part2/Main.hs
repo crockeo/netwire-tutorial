3d977ea16d918d379803bed2386c33ed4c8935bd
Crockeo <crockeo@gmail.com>
2014-09-09 11:22:35 -0400

    Finished the src for part 2.

--- src/part2/Main.hs
+++ src/part2/Main.hs
@@ -10,37 +10,43 @@ import Control.Wire
 import FRP.Netwire
 import Data.IORef
 import Linear.V2
-import Debug.Trace
 
 ----------
 -- Code --
 
 {-|
-  The size of the quad.
+  The size of the quad. Technically half of the size of the quad, as its
+  rendering is performed like so:
+
+    (x - s, y - s)
+    (x + s, y - s)
+    (x + s, y + s)
+    (x - s, y + s)
 -}
 s :: Float
 s = 0.05
 
 {-|
-  The global accelerational speed.
+  The global accelerational (and deceleration) speed.
 -}
 speed :: Float
-speed = 1.0
+speed = 4.0
 
 {-|
-  The minimum speed.
+  The minimum speed the quad can take before becoming completely stationary
+  again. This is used because otherwise, due to floating point inaccuracies,
+  you would have a quad shifting back and forth.
 -}
 minSpeed :: Float
 minSpeed = 0.01
 
 {-|
-  The maximum speed.
--}
-maxSpeed :: Float
-maxSpeed = 4.0
+  The initial position the quad takes. The bounds are:
 
-{-|
-  The initial position.
+    x min: -1 - s, x max: 1 + s
+    y min: -1 - s, y max: 1 + s
+
+  Where is is the global variable s stored above.
 -}
 initPos :: V2 Float
 initPos = pure 0
@@ -52,27 +58,26 @@ initPos = pure 0
 isKeyDown :: (Monoid e, Enum k) => k -> Wire s e IO a a
 isKeyDown k =
   mkGen_ $ \a -> do
-    s <- getKey k
-    return $ case s of
+    state <- getKey k
+    return $ case state of
       Release -> Left  mempty
       Press   -> Right a
-{-|
-  Wire for if the key to go left is down specifically.
--}
-leftKeyDown :: Monoid e => Wire s e IO a a
-leftKeyDown = isKeyDown $ CharKey 'A'
 
 {-|
-  Wire for if the key to go right is down specifically.
--}
-rightKeyDown :: Monoid e => Wire s e IO a a
-rightKeyDown = isKeyDown $ CharKey 'D'
-
-{-|
-  Performing some action with an input.
+  Performing some action with an input. Essentially a short hand version of
+  mkPure_ where you don't need to specify that something will not block.
 -}
 withInput :: (a -> b) -> Wire s e m a b
 withInput fn = mkPure_ $ \a -> Right $ fn a
+
+{-|
+  Applying deceleration to a @'Float'@.
+-}
+decel :: Float -> Float
+decel x
+  | x <  (-minSpeed)                     = ( speed)
+  | x >  ( minSpeed)                     = (-speed)
+  | otherwise                            = x
 
 {-|
   Generating two different directional acceleration / deceleration functions.
@@ -82,42 +87,35 @@ dAcceleration k1 k2  =  withInput decel . isKeyDown k1 . isKeyDown k2
                     <|> pure ( speed)   . isKeyDown k1
                     <|> pure (-speed)   . isKeyDown k2
                     <|> withInput decel
-  where decel :: Float -> Float
-        decel x
-          | x <  (-minSpeed)                     = ( speed)
-          | x >  ( minSpeed)                     = (-speed)
-          | x >= (-minSpeed) && x <= ( minSpeed) = (-x)
-
 
 {-|
-  The acceleration of the quad.
--}
-acceleration :: Monoid e => Wire s e IO Float Float
-acceleration  =  withInput decel . leftKeyDown . rightKeyDown
-             <|> pure (-speed)   . leftKeyDown
-             <|> pure ( speed)   . rightKeyDown
-             <|> withInput decel
-  where decel :: Float -> Float
-        decel x
-          | x < (-minSpeed)     = ( speed)
-          | x > ( minSpeed)     = (-speed)
-          | x > (-minSpeed) && x < ( minSpeed) = (-x)
-          | otherwise = 0
-
-{-|
-  The velocity of the quad.
+  The velocity of the quad. Checks for the minimum velocity on the acceleration
 -}
 velocity :: (HasTime t s, Monoid e) => Wire s e IO (Float, Float) (V2 Float)
-velocity = integral 0 . withInput (uncurry V2)
+velocity =
+  withInput stop . integral 0 . withInput (uncurry V2)
+  where stop :: V2 Float -> V2 Float
+        stop = fmap stopF
+          where stopF :: Float -> Float
+                stopF x =
+                  if x > (-minSpeed) && x < minSpeed
+                    then 0
+                    else x
 
 {-|
-  The position on the quad.
+  The current position of the quad. It returns the integral of the input of a
+  @'V2'@ @'Float'@. The input should be the current velocity of the block.
 -}
 position :: (HasTime t s, Monoid e) => Wire s e IO (V2 Float) (V2 Float)
-position = integral 0
+position = integral initPos
 
 {-|
-  The final position of the quad.
+  The final position of the quad. It uses Arrow notation, specifically the rec
+  keyword, to recursively define the application of the network. To get the
+  delta x, you need to know the current x velocity, and to get the current x
+  velocity you need to know the delta x. Conveniently, you can perform this
+  sort of recursive definition very easily using Arrow notation, as displayed
+  below.
 -}
 fPos :: HasTime t s => Wire s () IO a (V2 Float)
 fPos = proc _ -> do
@@ -145,7 +143,8 @@ runNetwork' closedRef session wire = do
           clear [ColorBuffer]
 
           renderPrimitive Quads $
-            mapM_ (\(V2 x y)-> vertex $ Vertex2 (realToFrac x :: GLfloat) (realToFrac y :: GLfloat))
+            mapM_ (\(V2 rx ry) -> vertex $ Vertex2 (realToFrac rx :: GLfloat)
+                                                   (realToFrac ry :: GLfloat))
                   [ V2 (x - s) (y - s)
                   , V2 (x + s) (y - s)
                   , V2 (x + s) (y + s)
